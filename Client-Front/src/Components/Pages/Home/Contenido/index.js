722941a8ad12723a24c5d3266fce810c839c6f07
saracarolina12 <0226594@up.edu.mx>
2021-11-19 21:46:42 -0600

    '19/11/2021'

--- Client-Front/src/Components/Pages/Home/Contenido/index.js
+++ Client-Front/src/Components/Pages/Home/Contenido/index.js
@@ -46,59 +46,6 @@ function ContenidoHome() {
                 </Col> 
             </Row>
             </div>
-
-            
-{/* 
-        <div className ="textoDiv">
-            <center>
-            <Carousel>
-                <Carousel.Item interval={Time}>
-                    <img
-                    className="d-block w-50 images"
-                    src={first_car}
-                    />
-                    <Carousel.Caption>
-                    <p className="tituloColumnas">¡Se eco-friendly!</p>
-                    <p className="texto">
-                        Este proyecto esta pensado para ayudar a reducir las emisiones de carbono de nuestra ciudad 
-                        y ayudar a todo aquel alumno de la UP que necesite ride.
-                        <br/> <br/>
-                        ¡Al participar, apoyas a salvar nuestro planeta!
-                    </p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item interval={Time}>
-                    <img
-                    className="d-block w-50 images"
-                    src={second_car}
-                    />
-                    <Carousel.Caption>
-                    <p className="tituloColumnas">Recibe horas de servicio social</p>
-                    <p className="texto">
-                        Al seleccionar ser conductor, habrá un contador de horas que te ayuda a completar
-                        tu servicio social.
-                        <br/> <br/>
-                        Entre más viajes des, más horas de servicio social podrás obtener
-                    </p>   
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item interval={Time}>
-                    <img
-                    className="d-block w-50 images"
-                    src={third_car}
-                    />
-                    <Carousel.Caption>
-                    <p className="tituloColumnas">¿Cómo comenzar?</p>
-                    <p className="texto">
-                        Regístrate con tu cuenta de la UP, inicia sesión, decide si quires ser conductor(dar
-                        ride) o pasajero (pedir viaje) y ¡Listo! <br/> <br/>
-                        Puedes encontrar más información en las páginas de Pedir viaje y Dar ride
-                    </p>   
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
-            </center>
-        </div>*/}
       </div> 
     )
 }
